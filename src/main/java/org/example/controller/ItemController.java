b549abfcc4561b006a8f1b9d8ce43f0fd09e2732
Divya <bellamkondabdivya@gmail.com>
2026-02-10 17:56:31 +0530

    added comment for redeployment

--- src/main/java/org/example/controller/ItemController.java
+++ src/main/java/org/example/controller/ItemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/items")
 public class ItemController {
-
+//Redeploy
     private final ItemService itemService;
 
     public ItemController(ItemService itemService) {
